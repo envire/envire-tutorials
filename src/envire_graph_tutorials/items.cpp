5fbec33e8c50cb6d46a7daca863c7b41a6ffc7b3
Arne Böckmann <arne.boeckmann@dfki.de>
2016-05-26 14:15:19 +0200

    improved

--- src/envire_graph_tutorials/items.cpp
+++ src/envire_graph_tutorials/items.cpp
@@ -1,12 +1,13 @@
 #include <envire_core/graph/EnvireGraph.hpp>
+//#snippet_begin:graph_items_classloader_includes
 #include <envire_core/plugin/ClassLoader.hpp>
 #include <envire_octomap/OcTree.hpp>
-
+//#snippet_end:graph_items_classloader_includes
 using namespace envire::core;
 
 int main(int argc, char **argv)
 {
-  //#snippet_begin:graph_items_setup
+  
   const FrameId frame = "I am so smart, S M A T";
   EnvireGraph g;
   g.addFrame(frame);
@@ -20,11 +21,12 @@ int main(int argc, char **argv)
     return -1;
   }
   //#snippet_end:graph_items_load_octree
-  
+  //#snippet_begin:graph_items_add_to_frame
   g.addItemToFrame(frame, octree);
-  
+  //#snippet_end:graph_items_add_to_frame
+  //#snippet_begin:graph_items_setFrame
   octree->setFrame(frame);
   g.addItem(octree);
-  
+  //#snippet_end:graph_items_setFrame
   
 }
