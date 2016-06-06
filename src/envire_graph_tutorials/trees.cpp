bd8b4b416f6e6e825db99831f2788c1e82e51382
Arne Böckmann <arne.boeckmann@dfki.de>
2016-06-06 15:17:36 +0200

    blabla

--- src/envire_graph_tutorials/trees.cpp
+++ src/envire_graph_tutorials/trees.cpp
@@ -6,13 +6,18 @@ using namespace envire::core;
 
 int main(int argc, char **argv)
 {
-  //#snippet_begin:graph_trees_get
+  //#snippet_begin:graph_trees_get_1
   EnvireGraph g;
   const FrameId root("root");
   /*
    ....
    */
-  TreeView view;
+  TreeView view = g.getTree(root);
+  //#snippet_end:graph_trees_get_1
+    //#snippet_begin:graph_trees_get_2
+  g.getTree(root, &view);
+  //#snippet_end:graph_trees_get_2
+    //#snippet_begin:graph_trees_get_3
   g.getTree(root, true, &view);
-  //#snippet_end:graph_trees_get
+  //#snippet_end:graph_trees_get_3
 }
