9726467a6191b2057e3f949a48eaa5f77bcdd69a
Arne Böckmann <arne.boeckmann@dfki.de>
2016-06-06 14:19:51 +0200

    improved

---
+++ src/envire_graph_tutorials/transforms.cpp
@@ -0,0 +1,38 @@
+#include <envire_core/graph/EnvireGraph.hpp>
+
+using namespace envire::core;
+
+
+
+int main(int argc, char **argv)
+{
+  //#snippet_begin:graph_transforms_add
+  EnvireGraph g;
+  const FrameId a = "frame_a";
+  const FrameId b = "frame_b";
+  Transform ab;
+  /** initialize Transform */
+  g.addTransform(a, b, ab);
+  //#snippet_end:graph_transforms_add
+  
+  //#snippet_begin:graph_transforms_remove
+  g.removeTransform(a, b);
+  //#snippet_end:graph_transforms_remove
+  
+  //#snippet_begin:graph_transforms_update
+  Transform tf;
+  tf.transform.translation << 84, 21, 42;
+  g.updateTransform(a, b, tf);
+  //#snippet_end:graph_transforms_update
+  
+  //#snippet_begin:graph_transforms_get
+  const Transform tf2 = g.getTransform(a, b);
+  //#snippet_end:graph_transforms_get
+
+  //#snippet_begin:graph_transforms_tree_get
+  TreeView view = g.getTree(graph.getVertex(a));
+  const Transform tf3 = g.getTransform(a, b, view);
+  //#snippet_end:graph_transforms_tree_get
+  
+
+}
